3011e8edcee24c7892f340c2f4f49503a42a016c
lohith.r <lohith.r@maersk.com>
2020-08-28 16:15:28 +0530

    adding reset and revert

--- src/javaoops/interfaces/FunctionalInter.java
+++ src/javaoops/interfaces/FunctionalInter.java
@@ -3,7 +3,5 @@ package javaoops.interfaces;
 public class FunctionalInter {
 
 	
-	// Practing the functional interface for practing
-	
-	String name = "lohith";
+	// Practing the functional interface
 }
