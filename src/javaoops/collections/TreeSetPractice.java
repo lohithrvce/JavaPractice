ff1434125c223928d175ec874ddaa9f6bd89a8bf
lohith.r <lohith.r@maersk.com>
2020-11-06 13:14:11 +0530

    added list and set collection practice code

--- src/javaoops/collections/TreeSetPractice.java
+++ src/javaoops/collections/TreeSetPractice.java
@@ -5,16 +5,25 @@ import java.util.TreeSet;
 public class TreeSetPractice {
 
 	public static void main(String[] args) {
-		TreeSet<Integer> ts = new TreeSet<Integer>();
+		TreeSet<Integer> ts = new TreeSet<Integer>(new MyComparator());
 		ts.add(23);
 		ts.add(12);
 		ts.add(234);
 		ts.add(2);
 		ts.add(9);
 		
-		System.out.println(ts.headSet(234));
+		//System.out.println(ts.headSet(234));
 		System.out.println(ts.hashCode());
 		
+		System.out.println("A".compareTo("A"));
+		System.out.println(ts);
+	
+		TreeSet<String> tstring = new TreeSet<String>(new MyStringComparator());
 		
+		tstring.add("lohith");
+		tstring.add("sharan");
+		tstring.add("deepu");
+		tstring.add("thummu");
+		System.out.println(tstring);
 	}
 }
